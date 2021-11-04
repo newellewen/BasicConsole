48b5766646fec7d7ce187839b201f0497db024cf
Phil Newell <newell.philip.86@gmail.com>
2021-11-04 09:48:11 -0400

    Adding implementations for methods

---
+++ Model/BasicConsoleModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace BasicConsole.Model
+{
+    public class BasicConsoleModel
+    {
+        public string Name { get; set; }
+        public int? Value { get; set; }
+    }
+}
--- Service/BasicConsoleService.cs
+++ Service/BasicConsoleService.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using BasicConsole.Configuration;
+using BasicConsole.Model;
 
 namespace BasicConsole.Service
 {
     public class BasicConsoleService : IBasicConsoleService
     {
         private readonly BasicConsoleOptions _basicConsoleOptions;
-        public List<Tuple<string, int?>> Variables { get; private set; }
+        public List<BasicConsoleModel> Variables { get; private set; }
 
         public BasicConsoleService(IOptions<BasicConsoleOptions> basicConsoleOptions)
         {
@@ -19,32 +19,95 @@ namespace BasicConsole.Service
 
         public void AddNewVariable(string name)
         {
-
+            if (Variables.Where(v => v.Name == name).Any())
+            {
+                Console.WriteLine($"The variable {name} already exists.");
+            }
+            else
+            {
+                Variables.Add(new BasicConsoleModel { Name = name, Value = null});
+            }            
         }
 
         public void SetVariableValue(string name, int value)
         {
-
+            if (!Variables.Where(v => v.Name == name).Any())
+            {
+                Console.WriteLine($"The variable {name} does not exist.");
+            }
+            else
+            {
+                Variables.Find(v => v.Name == name).Value = value;
+            }
         }
         public void Add(string varName1, string varName2)
         {
+            var var1 = Variables.First(v => v.Name == varName1);
+            var var2 = Variables.First(v => v.Name == varName2);
 
+            if (var1 == null || var2 == null)
+            {
+                Console.WriteLine($"One or more of the variables provided does not exist: {varName1}, {varName2}");
+            }
+            else
+            {
+                Variables.Find(v => v.Name == varName1).Value += var2.Value;
+            }
         }
         public void Add(string varName1, int value)
         {
+            var var1 = Variables.First(v => v.Name == varName1);
 
+            if (var1 == null)
+            {
+                Console.WriteLine($"The variable provided does not exist: {varName1}.");
+            }
+            else
+            {
+                Variables.Find(v => v.Name == varName1).Value += value;
+            }
         }
+
         public void Subtract(string varName1, string varName2)
         {
+            var var1 = Variables.First(v => v.Name == varName1);
+            var var2 = Variables.First(v => v.Name == varName2);
 
+            if (var1 == null || var2 == null)
+            {
+                Console.WriteLine($"One or more of the variables provided does not exist: {varName1}, {varName2}");
+            }
+            else
+            {
+                Variables.Find(v => v.Name == varName1).Value -= var2.Value;
+            }
         }
         public void Subtract(string varName1, int value)
         {
+            var var1 = Variables.First(v => v.Name == varName1);
 
+            if (var1 == null)
+            {
+                Console.WriteLine($"The variable provided does not exist: {varName1}.");
+            }
+            else
+            {
+                Variables.Find(v => v.Name == varName1).Value -= value;
+            }
         }
+
         public void PrintVariable(string varName)
         {
+            var var1 = Variables.First(v => v.Name == varName);
 
+            if (var1 == null)
+            {
+                Console.WriteLine($"The variable provided does not exist: {varName}.");
+            }
+            else
+            {
+                Console.WriteLine($"{var1.Name}: {var1.Value} ");
+            }
         }
 
     }
