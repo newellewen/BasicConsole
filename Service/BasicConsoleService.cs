6fe8b1e2da8e19c41c5df0c0a54e586588f1e87a
Phil Newell <newell.philip.86@gmail.com>
2021-11-04 09:25:57 -0400

    Added basic methods for rules

---
+++ Service/BasicConsoleService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using BasicConsole.Configuration;
+
+namespace BasicConsole.Service
+{
+    public class BasicConsoleService : IBasicConsoleService
+    {
+        private readonly BasicConsoleOptions _basicConsoleOptions;
+        public List<Tuple<string, int?>> Variables { get; private set; }
+
+        public BasicConsoleService(IOptions<BasicConsoleOptions> basicConsoleOptions)
+        {
+            _basicConsoleOptions = basicConsoleOptions.Value;
+        }
+
+        public void AddNewVariable(string name)
+        {
+
+        }
+
+        public void SetVariableValue(string name, int value)
+        {
+
+        }
+        public void Add(string varName1, string varName2)
+        {
+
+        }
+        public void Add(string varName1, int value)
+        {
+
+        }
+        public void Subtract(string varName1, string varName2)
+        {
+
+        }
+        public void Subtract(string varName1, int value)
+        {
+
+        }
+        public void PrintVariable(string varName)
+        {
+
+        }
+
+    }
+}
---
+++ Service/IBasicConsoleService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicConsole.Service
+{
+    interface IBasicConsoleService
+    {
+        void AddNewVariable(string name);
+        void SetVariableValue(string name, int value);
+        void Add(string varName1, string varName2);
+        void Add(string varName1, int value);
+        void Subtract(string varName1, string varName2);
+        void Subtract(string varName1, int value);
+        void PrintVariable(string varName);
+    }
+}
